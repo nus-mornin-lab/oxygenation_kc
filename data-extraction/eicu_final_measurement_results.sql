e7ac2a0aa44a6543e8c1f69f7acd7bd5430e5179
Willem van den Boom <41558513+willemvandenboom@users.noreply.github.com>
2018-08-08 14:49:01 +0800

    Update eicu_final_measurement_results.sql

--- data-extraction/eicu_final_measurement_results.sql
+++ data-extraction/eicu_final_measurement_results.sql
@@ -19,5 +19,6 @@ FROM pc
 INNER JOIN chart
   ON chart.patientunitstayid = pc.icustay_id 
 WHERE chart.nursingchartcelltypevalname = "O2 Saturation"
+/* The following selection can also be done in R to investigate any data quality issues
 AND chart.nursingchartoffset / (24 * 60) > 0
-AND chart.nursingchartoffset / (24 * 60) < pc.icu_length_of_stay
+AND chart.nursingchartoffset / (24 * 60) < pc.icu_length_of_stay */
