27d4954f3cba6ebe050ca3471866babcfb621cfc
Willem van den Boom <41558513+willemvandenboom@users.noreply.github.com>
2018-08-16 14:13:50 +0800

    Remove patient_cohort
    Remove patient cohort from this shell script now it no longer exists.

--- data-extraction/sync_to_bq.sh
+++ data-extraction/sync_to_bq.sh
@@ -1,7 +1,5 @@
-bq rm -f -t oxygenators-209612:eicu.patient_cohort
 bq rm -f -t oxygenators-209612:eicu.final_measurement_results
 bq rm -f -t oxygenators-209612:eicu.final_patient_results
 
-bq mk --use_legacy_sql=false --view "$(cat eicu_patient_cohort.sql)" oxygenators-209612:eicu.patient_cohort
 bq mk --use_legacy_sql=false --view "$(cat eicu_final_measurement_results.sql)" oxygenators-209612:eicu.final_measurement_results
 bq mk --use_legacy_sql=false --view "$(cat eicu_final_patient_results.sql)" oxygenators-209612:eicu.final_patient_results
