276d7726f582069c22dd0a750ab10b9aca436f3c
Michael Hoy <mch.hoy@gmail.com>
2019-02-05 15:48:06 +0800

    Refactored mimic code

--- MIMIC/data-extraction/patients_results_intermediate_5.sql
+++
@@ -1,6 +0,0 @@
-SELECT PIR4.*, 
-       SOFA.sofa
-FROM `oxygenators-209612.mimiciii_clinical.patients_Results_Intermediate_4` PIR4
-LEFT OUTER JOIN `oxygenators-209612.mimiciii_clinical.sofa` SOFA 
-ON PIR4.patient_id = SOFA.subject_id 
-AND PIR4.icustay_id = SOFA.icustay_id ; 
