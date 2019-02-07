af9533f2d2d0a91be1b9081913ab71c85acfe0b2
Willem van den Boom <vandenboom@nus.edu.sg>
2019-02-07 17:11:42 +0800

    Removed `rm` for tables that no longer exist

--- MIMIC/data-extraction/sync_to_bq.sh
+++ MIMIC/data-extraction/sync_to_bq.sh
@@ -1,14 +1,6 @@
-bq rm -f -t oxygenators-209612:mimiciii_clinical.patient_cohort
 bq rm -f -t oxygenators-209612:mimiciii_clinical.icd_codes
 bq rm -f -t oxygenators-209612:mimiciii_clinical.fluid_balance
 bq rm -f -t oxygenators-209612:mimiciii_clinical.mechanical_ventilative_volume
-bq rm -f -t oxygenators-209612:mimiciii_clinical.patients_Results_Intermediate_1
-bq rm -f -t oxygenators-209612:mimiciii_clinical.patients_Results_Intermediate_2
-bq rm -f -t oxygenators-209612:mimiciii_clinical.patients_Results_Intermediate_3
-bq rm -f -t oxygenators-209612:mimiciii_clinical.patients_Results_Intermediate_4
-bq rm -f -t oxygenators-209612:mimiciii_clinical.patients_Results_Intermediate_5
-bq rm -f -t oxygenators-209612:mimiciii_clinical.patients_Results_Intermediate_6
-bq rm -f -t oxygenators-209612:mimiciii_clinical.final_patients_results
 bq rm -f -t oxygenators-209612:mimiciii_clinical.measurements_results
 bq rm -f -t oxygenators-209612:mimiciii_clinical.mimic_final_patient_results
 
