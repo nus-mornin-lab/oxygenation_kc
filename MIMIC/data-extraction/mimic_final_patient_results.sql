11f6f12d99deb52bad9186cd8df9e3b596feff55
Willem van den Boom <vandenboom@nus.edu.sg>
2019-02-09 14:53:19 +0800

    Add height and weight to mimic_final_patient_results.sql

--- MIMIC/data-extraction/mimic_final_patient_results.sql
+++ MIMIC/data-extraction/mimic_final_patient_results.sql
@@ -52,7 +52,87 @@ AND ventsettings.icustay_id = icu.icustay_id)
 WHEN 0 THEN 0 
 ELSE 1 
 END AS invasive
-FROM `oxygenators-209612.mimiciii_clinical.icustays` AS icu)
+FROM `oxygenators-209612.mimiciii_clinical.icustays` AS icu),
+
+
+
+-- Edited from https://github.com/MIT-LCP/mimic-code/blob/master/concepts/demographics/HeightWeightQuery.sql
+-- This query gets the first weight and height
+-- for a single ICUSTAY_ID. It extracts data from the CHARTEVENTS table.
+heightweight AS (
+WITH FirstVRawData AS
+  (SELECT c.charttime,
+    c.itemid,c.subject_id,c.icustay_id,
+    CASE
+      WHEN c.itemid IN (762, 763, 3723, 3580, 3581, 3582, 226512)
+        THEN 'WEIGHT'
+      WHEN c.itemid IN (920, 1394, 4187, 3486, 3485, 4188, 226707)
+        THEN 'HEIGHT'
+    END AS parameter,
+    -- Ensure that all weights are in kg and heights are in centimeters
+    CASE
+      WHEN c.itemid   IN (3581, 226531)
+        THEN c.valuenum * 0.45359237
+      WHEN c.itemid   IN (3582)
+        THEN c.valuenum * 0.0283495231
+      WHEN c.itemid   IN (920, 1394, 4187, 3486, 226707)
+        THEN c.valuenum * 2.54
+      ELSE c.valuenum
+    END AS valuenum
+  FROM `oxygenators-209612.mimiciii_clinical.chartevents` c
+  WHERE c.valuenum   IS NOT NULL
+  -- exclude rows marked as error
+  AND (NOT (c.error <> 1 OR c.error IS NULL) OR (c.error IS NULL))  --c.error IS DISTINCT FROM 1
+  AND ( ( c.itemid  IN (762, 763, 3723, 3580, -- Weight Kg
+    3581,                                     -- Weight lb
+    3582,                                     -- Weight oz
+    920, 1394, 4187, 3486,                    -- Height inches
+    3485, 4188                                -- Height cm
+    -- Metavision
+    , 226707 -- Height (measured in inches)
+    , 226512 -- Admission Weight (Kg)
+
+    -- note we intentionally ignore the below ITEMIDs in metavision
+    -- these are duplicate data in a different unit
+    -- , 226531 -- Admission Weight (lbs.)
+    -- , 226730 -- Height (cm)
+    )
+  AND c.valuenum <> 0 )
+    ) )
+  --)
+
+  --select * from FirstVRawData
+, SingleParameters AS (
+  SELECT DISTINCT subject_id,
+         icustay_id,
+         parameter,
+         first_value(valuenum) over
+            (partition BY subject_id, icustay_id, parameter
+             order by charttime ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+             AS first_valuenum
+    FROM FirstVRawData
+
+--   ORDER BY subject_id,
+--            icustay_id,
+--            parameter
+  )
+--select * from SingleParameters
+, PivotParameters AS (SELECT subject_id, icustay_id,
+    MAX(case when parameter = 'HEIGHT' then first_valuenum else NULL end) AS height_first,
+    MAX(case when parameter = 'WEIGHT' then first_valuenum else NULL end) AS weight_first
+  FROM SingleParameters
+  GROUP BY subject_id,
+    icustay_id
+  )
+--select * from PivotParameters
+SELECT f.icustay_id,
+  f.subject_id,
+  ROUND( cast(f.height_first as numeric), 2) AS height_first,
+  ROUND(cast(f.weight_first as numeric), 2) AS weight_first
+
+FROM PivotParameters f)
+
+
 
 
 -- Note that icustays has duplicate icustay_id, need to check the final
@@ -81,7 +161,9 @@ elix.metastatic_cancer,
 angus.angus,
 sofa.sofa,
 fluid_balance.fluid_balance, 
-mech_vent.tidal_high_count2 as tidal_count_percentage
+mech_vent.tidal_high_count2 as tidal_count_percentage,
+heightweight.height_first as height,
+heightweight.weight_first as weight
 FROM `oxygenators-209612.mimiciii_clinical.icustays` AS icu
 INNER JOIN `oxygenators-209612.mimiciii_clinical.patients` AS pat
   ON icu.subject_id = pat.subject_id
@@ -101,6 +183,8 @@ LEFT JOIN `oxygenators-209612.mimiciii_clinical.fluid_balance` fluid_balance
   ON icu.icustay_id = fluid_balance.icustay_id
 LEFT JOIN `oxygenators-209612.mimiciii_clinical.mechanical_ventilative_volume` mech_vent 
   ON icu.icustay_id = mech_vent.icustay_id
+LEFT JOIN heightweight
+  ON icu.icustay_id = heightweight.icustay_id
 
 --Use this to validate non-duplicate icustay_id
 --SELECT test.icustay_id, count(test.icustay_id) as c FROM test GROUP BY test.icustay_id ORDER BY c DESC LIMIT 100
