7cd91c073b307d1d06552708d082ebda6b84f9a3
Willem van den Boom <vandenboom@nus.edu.sg>
2019-02-09 14:15:09 +0800

    Remove unit from measurements_results
    The unit is "%" for all SpO2 measurements. Therefore, the unit is not required.

--- MIMIC/data-extraction/measurements_results.sql
+++ MIMIC/data-extraction/measurements_results.sql
@@ -1,7 +1,6 @@
 SELECT DISTINCT 
 chart.icustay_id as icustay_id,
-chart.valuenum as spO2_Value, 
-chart.valueuom as sp02_Unit,
+chart.valuenum as spO2_Value,
 chart.charttime as measurement_time
 FROM `oxygenators-209612.mimiciii_clinical.chartevents` AS chart
 WHERE chart.itemid in (220277, 646) 
