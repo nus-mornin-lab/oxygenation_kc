bd07e4e54a49c512e21d488617b756f4e97aa60d
Willem van den Boom <vandenboom@nus.edu.sg>
2019-02-10 16:00:16 +0800

    Extract oxygen therapy information for MIMIC
    Added extraction on oxygen therapy sessions and their type (supplemental oxygen versus mechanical ventilation).
    Because I did not readily see how to do it, I did not split ventilation into noninvasive and invasive ventilation.

--- MIMIC/data-extraction/measurements_results.sql
+++ MIMIC/data-extraction/measurements_results.sql
@@ -1,7 +1,18 @@
-SELECT DISTINCT 
-chart.icustay_id as icustay_id,
+WITH ce AS (SELECT DISTINCT 
+chart.icustay_id,
 chart.valuenum as spO2_Value,
-chart.charttime as measurement_time
+chart.charttime
 FROM `oxygenators-209612.mimiciii_clinical.chartevents` AS chart
 WHERE chart.itemid in (220277, 646) 
 AND chart.valuenum IS NOT NULL
+-- exclude rows marked as error
+and (chart.error <> 1 OR chart.error IS NULL) --chart.error IS DISTINCT FROM 1
+)
+
+-- Edited from https://github.com/cosgriffc/hyperoxia-sepsis
+SELECT
+    ce.icustay_id,
+    ce.spO2_Value
+FROM ce
+INNER JOIN `oxygenators-209612.mimiciii_clinical.mimic_oxygen_therapy` vd ON ce.icustay_id = vd.icustay_id
+WHERE vd.vent_start <= ce.charttime AND vd.vent_end >= ce.charttime AND vd.ventnum_seq = 1
--- MIMIC/data-extraction/sync_to_bq.sh
+++ MIMIC/data-extraction/sync_to_bq.sh
@@ -3,9 +3,11 @@ bq rm -f -t oxygenators-209612:mimiciii_clinical.fluid_balance
 bq rm -f -t oxygenators-209612:mimiciii_clinical.mechanical_ventilative_volume
 bq rm -f -t oxygenators-209612:mimiciii_clinical.measurements_results
 bq rm -f -t oxygenators-209612:mimiciii_clinical.mimic_final_patient_results
+bq rm -f -t oxygenators-209612:mimiciii_clinical.mimic_oxygen_therapy
 
 bq mk --use_legacy_sql=false --view "$(cat icd_codes.sql)" oxygenators-209612:mimiciii_clinical.icd_codes
 bq mk --use_legacy_sql=false --view "$(cat fluid_balance.sql)" oxygenators-209612:mimiciii_clinical.fluid_balance
 bq mk --use_legacy_sql=false --view "$(cat mechanical_ventilative_volume.sql)" oxygenators-209612:mimiciii_clinical.mechanical_ventilative_volume
 bq mk --use_legacy_sql=false --view "$(cat mimic_final_patient_results.sql)" oxygenators-209612:mimiciii_clinical.mimic_final_patient_results
+bq mk --use_legacy_sql=false --view "$(cat mimic_oxygen_therapy.sql)" oxygenators-209612:mimiciii_clinical.mimic_oxygen_therapy
 bq mk --use_legacy_sql=false --view "$(cat measurements_results.sql)" oxygenators-209612:mimiciii_clinical.measurements_results
