cc7d7e014280dd29b1f2b8887dc87137fb77d573
Willem van den Boom <vandenboom@nus.edu.sg>
2019-02-15 11:10:20 +0800

    Merge `measurements_results` into `mimic_final_patient_results`

--- MIMIC/data-extraction/mimic_final_patient_results.sql
+++ MIMIC/data-extraction/mimic_final_patient_results.sql
@@ -41,6 +41,37 @@ GROUP BY chart.icustay_id),
 
 
 
+-- Computing summaries of the blood oxygen saturation (SpO2)
+SpO2 AS (
+
+WITH ce AS (SELECT DISTINCT 
+chart.icustay_id,
+chart.valuenum as spO2_Value,
+chart.charttime
+FROM `oxygenators-209612.mimiciii_clinical.chartevents` AS chart
+WHERE chart.itemid in (220277, 646) 
+AND chart.valuenum IS NOT NULL
+-- exclude rows marked as error
+and (chart.error <> 1 OR chart.error IS NULL) --chart.error IS DISTINCT FROM 1
+-- We remove oxygen measurements that are outside of the range [10, 100]
+AND chart.valuenum >= 10 AND chart.valuenum <= 100
+)
+
+-- Edited from https://github.com/cosgriffc/hyperoxia-sepsis
+SELECT DISTINCT
+    ce.icustay_id
+  , COUNT(ce.spO2_Value) OVER(PARTITION BY ce.icustay_id) AS nOxy
+  , PERCENTILE_CONT(ce.spO2_Value, 0.5) OVER(PARTITION BY ce.icustay_id) AS median
+  , AVG(CAST(ce.spO2_Value < 94 AS INT64)) OVER(PARTITION BY ce.icustay_id) AS propBelow
+  , AVG(CAST(ce.spO2_Value > 97 AS INT64)) OVER(PARTITION BY ce.icustay_id) AS propAbove
+FROM ce
+INNER JOIN oxygen_therapy ON ce.icustay_id = oxygen_therapy.icustay_id
+WHERE oxygen_therapy.vent_start <= ce.charttime AND oxygen_therapy.vent_end >= ce.charttime
+
+),
+
+
+
 -- Edited from https://github.com/MIT-LCP/mimic-code/blob/master/concepts/demographics/HeightWeightQuery.sql
 -- This query gets the first weight and height
 -- for a single ICUSTAY_ID. It extracts data from the CHARTEVENTS table.
@@ -172,6 +203,7 @@ CASE
 END AS first_stay,
 oxygen_therapy.* EXCEPT(icustay_id)
 , fiO2.max_fiO2
+, SpO2.* EXCEPT(icustay_id)
 FROM icu
 LEFT JOIN pat
   ON icu.subject_id = pat.subject_id
@@ -195,3 +227,5 @@ LEFT JOIN oxygen_therapy
   ON icu.icustay_id = oxygen_therapy.icustay_id
 LEFT JOIN fiO2
   ON icu.icustay_id = fiO2.icustay_id
+LEFT JOIN SpO2
+  ON icu.icustay_id = SpO2.icustay_id
