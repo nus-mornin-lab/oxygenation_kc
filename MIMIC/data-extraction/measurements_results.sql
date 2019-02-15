9df2068b804bc2dc3594830c13bd95c26620cabd
Willem van den Boom <vandenboom@nus.edu.sg>
2019-02-15 11:05:22 +0800

    Compute summaries of SpO2 inside `measurements_results`
    Computing summaries of the blood oxygen saturation (SpO2) inside SQL rather than R yields a huge speedup.

--- MIMIC/data-extraction/measurements_results.sql
+++ MIMIC/data-extraction/measurements_results.sql
@@ -7,12 +7,17 @@ WHERE chart.itemid in (220277, 646)
 AND chart.valuenum IS NOT NULL
 -- exclude rows marked as error
 and (chart.error <> 1 OR chart.error IS NULL) --chart.error IS DISTINCT FROM 1
+-- We remove oxygen measurements that are outside of the range [10, 100]
+AND chart.valuenum >= 10 AND chart.valuenum <= 100
 )
 
 -- Edited from https://github.com/cosgriffc/hyperoxia-sepsis
-SELECT
-    ce.icustay_id,
-    ce.spO2_Value
+SELECT DISTINCT
+    ce.icustay_id
+  , COUNT(ce.spO2_Value) OVER(PARTITION BY ce.icustay_id) AS nOxy
+  , PERCENTILE_CONT(ce.spO2_Value, 0.5) OVER(PARTITION BY ce.icustay_id) AS median
+  , AVG(CAST(ce.spO2_Value < 94 AS INT64)) OVER(PARTITION BY ce.icustay_id) AS propBelow
+  , AVG(CAST(ce.spO2_Value > 97 AS INT64)) OVER(PARTITION BY ce.icustay_id) AS propAbove
 FROM ce
 INNER JOIN `oxygenators-209612.mimiciii_clinical.mimic_oxygen_therapy` vd ON ce.icustay_id = vd.icustay_id
-WHERE vd.vent_start <= ce.charttime AND vd.vent_end >= ce.charttime AND vd.ventnum_seq = 1
+WHERE vd.vent_start <= ce.charttime AND vd.vent_end >= ce.charttime
